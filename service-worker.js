e9e7665a73edae11acfa664950b12e74fcd7abd0
kelvinikome <kvnikome@gmail.com>
2018-07-01 17:57:44 +0100

    offline first feature testing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,35 @@
-self.addEventListener( "fetch" , function (event) {
-    //fetch request as specified by event object 
-    console.log(event.request); //Note that Request and Response are also objects 
+
+self.addEventListener( "install" , function (event) { 
+    event.waitUntil(
+        caches.open("cacheName1").then(function(cache) {
+            return cache.addAll(
+                [
+                    '/',
+                    '/css/style.css',
+                    '/js/converter.js',
+                    '/js/idb.js',
+                    '/js/main.js',
+                    '/img/ddw-currency-converter.png'
+                ]
+            );
+        })
+    );
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request.url).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request.url) 
+      }).catch(function(error) {
+  
+        // Respond with custom offline page
+  
+      })
+    );
+  });
